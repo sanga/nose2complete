8f550e7b3da0b3cf6f2691d3bf209f1719f404fa
Tim Sampson <timothy.sampson@f-secure.com>
2013-05-28 07:01:10 +0000

    initial code drop

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='nose2complete',
+      version='0.1.0',
+      description='nose2complete is a plugin used for completing test names from the command line. Heavily inspired by nosecomplete',
+      author='Tim Sampson',
+      author_email='littlesanga@gmail.com',
+      url='https://github.com/sanga/nose2complete',
+      license='MIT',
+      py_modules=['nose2complete'],
+      install_requires=['python-nose2'],
+      entry_points={'console_scripts': ['nose2complete=nose2complete:main']})
